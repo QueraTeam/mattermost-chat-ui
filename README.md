38fd634ba213ac59bafc6667fc7853037f5d7277
Mohammad Javad Naderi <mjnaderi@gmail.com>
2020-03-06 18:00:48 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Mattermost Chat UI
+
+This plugin reduces width of message boxes
+and applies other UI customizations to Mattermost chat messages.
+
+**Before** enabling plugin:
+
+![before](screenshots/screenshot2.png)
+
+**After** enabling plugin:
+
+![after](screenshots/screenshot3.png)
+
+This repository is based on
+[mattermost/mattermost-plugin-starter-template](https://github.com/mattermost/mattermost-plugin-starter-template).
+
+## Installation
+
+Download the plugin file (`ir.quera.mattermost-chat-ui-*.tar.gz`) from
+[Releases](https://github.com/QueraTeam/mattermost-chat-ui/releases)
+page and upload to your Mattermost
+server via **System Console**.
+
+## Other plugins:
+
+- [mattermost-rtl](https://github.com/QueraTeam/mattermost-rtl)
+
+## Development, Build
+
+    make
---
+++ webapp/src/index.js
@@ -0,0 +1,25 @@
+import manifest from './manifest';
+import "./style.scss"
+
+class MattermostChatUIPlugin {
+    initialize(registry, store) {
+        console.info('Hi! Mattermost Chat UI plugin initialized...');
+        setInterval(this.detectDarkTheme, 5000);
+    }
+
+    detectDarkTheme() {
+        const rgb = getComputedStyle(
+            document.getElementById('app-content')
+        ).backgroundColor.split('(')[1].split(')')[0].split(', ').map(Number);
+        const luma = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
+        if (luma < 140) {
+            document.body.classList.remove('q--light-theme');
+            document.body.classList.add('q--dark-theme');
+        } else {
+            document.body.classList.remove('q--dark-theme');
+            document.body.classList.add('q--light-theme');
+        }
+    }
+}
+
+window.registerPlugin(manifest.id, new MattermostChatUIPlugin());
