ae5a954733bf0e14fa6f634bd498563c8113268b
Mohammad Javad Naderi <mjnaderi@gmail.com>
2023-07-29 13:06:08 +0330

    Update dependencies and add eslint

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,27 +1,29 @@
 import manifest from './manifest';
-import "./style.scss"
+import './style.scss';
+
+function detectDarkTheme() {
+  const el = document.getElementById('app-content');
+  if (el === null) return;
+  const rgb = getComputedStyle(el).backgroundColor
+    .split('(')[1].split(')')[0].split(', ').map(Number);
+  const luma = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
+  if (luma < 140) {
+    document.body.classList.remove('q--light-theme');
+    document.body.classList.add('q--dark-theme');
+  } else {
+    document.body.classList.remove('q--dark-theme');
+    document.body.classList.add('q--light-theme');
+  }
+}
 
 class MattermostChatUIPlugin {
-    initialize(registry, store) {
-        console.info('Hi! Mattermost Chat UI plugin initialized...');
-        setInterval(this.detectDarkTheme, 5000);
-    }
+  initialize() {
+    this.interval = setInterval(detectDarkTheme, 5000);
+  }
 
-    detectDarkTheme() {
-        const el = document.getElementById('app-content');
-        if (el === null)
-            return;
-        const rgb = getComputedStyle(el).backgroundColor
-            .split('(')[1].split(')')[0].split(', ').map(Number);
-        const luma = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
-        if (luma < 140) {
-            document.body.classList.remove('q--light-theme');
-            document.body.classList.add('q--dark-theme');
-        } else {
-            document.body.classList.remove('q--dark-theme');
-            document.body.classList.add('q--light-theme');
-        }
-    }
+  uninitialize() {
+    clearInterval(this.interval);
+  }
 }
 
 window.registerPlugin(manifest.id, new MattermostChatUIPlugin());
